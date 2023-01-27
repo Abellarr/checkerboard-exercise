b38f8e02fd4ab74f9118450d8ab13c9a07e867e0
Abellarr <da.ortiz83@gmail.com>
2023-01-26 16:42:46 -0800

    Updated for Black/Red

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Your JS goes here
+// function checkerBoard() {
+for (let i = 0; i < 81; i++) {
+    // let randomColor = Math.floor(Math.random()*16777215).toString(16)
+    let tile = document.createElement('div')
+    tile.classList.add("tile")
+    tile.style.width = "11.1%"
+    tile.style.paddingBottom = "11.1%"
+    tile.style.float = "left"
+    if (i%2 === 0) {
+        tile.style.backgroundColor = 'black'
+    } else {
+        tile.style.backgroundColor = 'red'
+    }
+    // console.log(tile)
+    document.body.appendChild(tile)
+}
+
+
+
+
+// }
+
+// checkerBoard()
+
+// 
