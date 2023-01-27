5d1981672bb6c43d64dcd0275359f1d0c5061945
Abellarr <da.ortiz83@gmail.com>
2023-01-27 15:12:54 -0800

    Checkerboard with random colors. Tested and working.

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // function checkerBoard() {
 for (let i = 0; i < 81; i++) {
-    // let randomColor = Math.floor(Math.random()*16777215).toString(16)
+    let randomColor = Math.floor(Math.random()*16777215).toString(16) // variable to compute hexadecimal color code
     let tile = document.createElement('div')
     tile.classList.add("tile")
     tile.style.width = "11.1%"
     tile.style.paddingBottom = "11.1%"
     tile.style.float = "left"
-    if (i%2 === 0) {
-        tile.style.backgroundColor = 'black'
-    } else {
-        tile.style.backgroundColor = 'red'
-    }
-    // console.log(tile)
+    tile.style.backgroundColor = '#'+randomColor;
+    
+    // if (i%2 === 0) {                         // coding for black and red squares
+    //     tile.style.backgroundColor = 'black'
+    // } else {
+    //     tile.style.backgroundColor = 'red'
+    // }
+    console.log(tile)  //<---for testing purposes
     document.body.appendChild(tile)
 }
 
